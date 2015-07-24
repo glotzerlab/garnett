9766639b3ad56f9033428654ff8e78c8da661793
Carl Simon Adorf <csadorf@umich.edu>
2015-07-24 13:21:26 -0400

    Add Unittests for the make_- and copyto_snapshot.

--- tests/test_trajectory.py
+++ tests/test_trajectory.py
@@ -1,10 +1,94 @@
+import sys
+import io
 import unittest
+import tempfile
 
 import glotzformats
 
+PYTHON_3 = sys.version_info[0] == 3
+
+try:
+    from hoomd_script import context
+except ImportError:
+    HOOMD = False
+else:
+    context.initialize()
+    HOOMD = True
+
 class TrajectoryTest(unittest.TestCase):
-    # no tests defined so far
-    pass
+
+    def read_trajectory(self, stream, precision=None):
+        reader = glotzformats.reader.PosFileReader(precision=precision)
+        return reader.read(stream)
+        traj = reader.read(io.StringIO(glotzformats.samples.POS_HPMC))
+
+    def get_trajectory(self, sample):
+        if PYTHON_3:
+            sample_file = io.StringIO(glotzformats.samples.POS_HPMC)
+        else:
+            sample_file = io.StringIO(unicode(glotzformats.samples.POS_HPMC))
+        return self.read_trajectory(sample_file) # account for low injavis precision
+
+
+@unittest.skipIf(not HOOMD, 'requires hoomd-blue')
+class FrameSnapshotExport(TrajectoryTest):
+
+    def make_snapshot(self, sample):
+        traj = self.get_trajectory(sample)
+        return traj[-1].make_snapshot()
+
+    def del_system(self):
+        del self.system
+
+    def del_mc(self):
+        del self.mc
+
+    def assert_snapshots_equal(self, s0, s1):
+        self.assertEqual(s0.particles.N, s1.particles.N)
+        self.assertEqual(s0.box.get_metadata(), s1.box.get_metadata())
+        self.assertTrue((s0.particles.position==s1.particles.position).all())
+        self.assertTrue((s0.particles.orientation==s1.particles.orientation).all())
+
+    def test_sphere(self):
+        from hoomd_script import init, sorter, data, dump, run
+        from hoomd_plugins import hpmc
+        self.system = init.create_empty(N=2, box=data.boxdim(L=10, dimensions=3), particle_types=['A'])
+        self.addCleanup(init.reset)
+        self.addCleanup(self.del_system)
+        self.mc = hpmc.integrate.sphere(seed=10);
+        self.mc.shape_param.set("A", diameter=1.0)
+        self.addCleanup(self.del_mc)
+        self.system.particles[0].position = (1,0,0);
+        self.system.particles[0].orientation = (1,0,0,0);
+        self.system.particles[1].position = (2,0,0);
+        self.system.particles[1].orientation = (1,0,0,0);
+        sorter.set_params(grid=8)
+        with tempfile.NamedTemporaryFile('r') as tmpfile:
+            dump.pos(filename=tmpfile.name, period = 1)
+            run(10, quiet=True)
+            snapshot0 = self.system.take_snapshot()
+            run(1, quiet=True)  # the hoomd pos-writer lags by one sweep
+            tmpfile.flush()
+            traj = self.read_trajectory(tmpfile)
+            snapshot1 = traj[-1].make_snapshot()
+            self.assert_snapshots_equal(snapshot0, snapshot1)
+            self.system.restore_snapshot(snapshot1)
+
+    def test_hpmc_dialect(self):
+        snapshot = self.make_snapshot(glotzformats.samples.POS_HPMC)
+        self.assertEqual(snapshot.box.Lx, 10.0)
+        self.assertEqual(snapshot.box.Ly, 10.0)
+        self.assertEqual(snapshot.box.Lz, 10.0)
+        self.assertEqual(snapshot.particles.types, ['A'] * 3)
+
+    def test_incsim_dialect(self):
+        snapshot = self.make_snapshot(glotzformats.samples.POS_INCSIM)
+
+    def test_monotype_dialect(self):
+        snapshot = self.make_snapshot(glotzformats.samples.POS_MONOTYPE)
+
+    def test_injavis_dialect(self):
+        snapshot = self.make_snapshot(glotzformats.samples.POS_INJAVIS)
 
 if __name__ == '__main__':
     unittest.main()
