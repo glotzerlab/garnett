3a17c920d97116c1eb316b92fa29ae5f2bbf5ff4
Carl Simon Adorf <csadorf@umich.edu>
2015-07-24 11:57:46 -0400

    Allow to set precision on pos-file reader init.

--- src/glotzformats/posfilereader.py
+++ src/glotzformats/posfilereader.py
@@ -24,12 +24,6 @@ POSFILE_FLOAT_DIGITS = 11
 COMMENT_CHARACTERS = ['//']
 TOKENS_SKIP = ['rotation', 'antiAliasing']
 
-def num(x):
-    if isinstance(x, int):
-        return x
-    else:
-        return round(float(x), POSFILE_FLOAT_DIGITS)
-
 def is_comment(line):
     for comment_char in COMMENT_CHARACTERS:
         if line.startswith(comment_char):
@@ -39,22 +33,19 @@ def is_comment(line):
 class PosFileReader(object):
     """Read pos-files with different dialects."""
 
-    def _read_data_section(self, header, stream):
-        """Read data section from stream."""
-        data = collections.defaultdict(list)
-        keys = header.strip().split()[1:]
-        for i, line in enumerate(stream):
-            if is_comment(line):
-                continue
-            if line.startswith('#[done]'):
-                return keys, data, i
-            else:
-                values = line.split()
-                for key, value in zip(keys, values):
-                    data[key].append(value)
+    def __init__(self, precision=None):
+        """Initialize a pos-file reader.
+
+        :param precision: The number of digits to round floating-point values to.
+        :type precision: int
+        """
+        self.precision=precision or POSFILE_FLOAT_DIGITS
+
+    def num(self, x):
+        if isinstance(x, int):
+            return x
         else:
-            warnings.warn("File ended abruptly.", ParserWarning)
-            return keys, data, i
+            return round(float(x), self.precision)
 
     def read(self, stream, default_type='A'):
         """Read text stream and return a trajectory instance.
@@ -99,21 +90,21 @@ class PosFileReader(object):
                     definition, data, end = line.strip().split('"')
                     _assert(len(end) == 0)
                     name = definition.split()[1]
-                    raw_frame.shapedef[name] = parse_shape_definition(data)
+                    raw_frame.shapedef[name] = self._parse_shape_definition(data)
                 elif tokens[0] == 'shape': # monotype
                     definition = line.strip().split('"')[1]
-                    raw_frame.shapedef[default_type] = parse_shape_definition(definition)
+                    raw_frame.shapedef[default_type] = self._parse_shape_definition(definition)
                     _assert(len(raw_frame.shapedef) == 1)
                     monotype = True
                 elif tokens[0] in ('boxMatrix', 'box'):
                     if len(tokens) == 10:
                     #_assert(len(tokens) == 10)
-                    #raw_frame.box = np.array((num(v) for v in tokens[1:])).reshape((3,3))
-                        raw_frame.box = np.array([num(v) for v in tokens[1:]]).reshape((3,3))
+                    #raw_frame.box = np.array((self.num(v) for v in tokens[1:])).reshape((3,3))
+                        raw_frame.box = np.array([self.num(v) for v in tokens[1:]]).reshape((3,3))
                     elif len(tokens) == 4:
                     #raw_frame.box = np.array(tokens[1:]).reshape((3,3))
                     #__assert(len(tokens) == 4)
-                        raw_frame.box = np.array([[num(tokens[1]),0,0],[0,num(tokens[2]),0],[0,0,num(tokens[3])]]).reshape((3,3))
+                        raw_frame.box = np.array([[self.num(tokens[1]),0,0],[0,self.num(tokens[2]),0],[0,0,self.num(tokens[3])]]).reshape((3,3))
                 else:
                     # assume we are reading positions now
                     if not monotype:
@@ -130,33 +121,50 @@ class PosFileReader(object):
                     else:
                         raise ParserError(line)
                     raw_frame.types.append(name)
-                    raw_frame.positions.append([num(v) for v in xyz])
-                    raw_frame.orientations.append([num(v) for v in quat])
+                    raw_frame.positions.append([self.num(v) for v in xyz])
+                    raw_frame.orientations.append([self.num(v) for v in quat])
         if len(frames) == 0:
             raise ParserError("Did not read a single complete frame.")
         logger.info("Read {} frames.".format(len(frames)))
         return Trajectory(frames)
 
-def parse_shape_definition(line):
-    try:
-        tokens = (t for t in line.split())
-        shape_class = next(tokens)
-        if shape_class.lower() == 'sphere':
-            diameter = float(next(tokens))
+    def _read_data_section(self, header, stream):
+        """Read data section from stream."""
+        data = collections.defaultdict(list)
+        keys = header.strip().split()[1:]
+        for i, line in enumerate(stream):
+            if is_comment(line):
+                continue
+            if line.startswith('#[done]'):
+                return keys, data, i
+            else:
+                values = line.split()
+                for key, value in zip(keys, values):
+                    data[key].append(value)
         else:
-            num_vertices = int(next(tokens))
-            vertices = []
-            for i in range(num_vertices):
-                xyz = next(tokens), next(tokens), next(tokens)
-                vertices.append([num(v) for v in xyz])
+            warnings.warn("File ended abruptly.", ParserWarning)
+            return keys, data, i
+
+    def _parse_shape_definition(self, line):
         try:
-            color = next(tokens)
-        except StopIteration:
-            color = None
-        if shape_class.lower() == 'sphere':
-            return SphereShapeDefinition(diameter=diameter,color=color)
-        else:
-            return PolyShapeDefinition(shape_class=shape_class,vertices=vertices,color=color)
-    except Exception as error:
-        warnings.warn("Failed to parse shape definition, using fallback mode. ({})".format(line))
-        return FallbackShapeDefinition(line)
+            tokens = (t for t in line.split())
+            shape_class = next(tokens)
+            if shape_class.lower() == 'sphere':
+                diameter = float(next(tokens))
+            else:
+                num_vertices = int(next(tokens))
+                vertices = []
+                for i in range(num_vertices):
+                    xyz = next(tokens), next(tokens), next(tokens)
+                    vertices.append([self.num(v) for v in xyz])
+            try:
+                color = next(tokens)
+            except StopIteration:
+                color = None
+            if shape_class.lower() == 'sphere':
+                return SphereShapeDefinition(diameter=diameter,color=color)
+            else:
+                return PolyShapeDefinition(shape_class=shape_class,vertices=vertices,color=color)
+        except Exception as error:
+            warnings.warn("Failed to parse shape definition, using fallback mode. ({})".format(line))
+            return FallbackShapeDefinition(line)
