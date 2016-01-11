b05551931d90e0d4a3ce597b787e1e947b35d6e7
Carl Simon Adorf <csadorf@umich.edu>
2016-01-11 14:11:42 -0500

    Update documentation to new iteration logic.

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,10 @@ from unittest.mock import MagicMock
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-        return Mock()
+        if name == '_mock_methods':
+            return cls._mock_methods
+        else:
+            return Mock()
 
 MOCK_MODULES = ['numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
