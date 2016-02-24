31abed886e64adf6b7c03521915bcac9eff6f3b4
Carl Simon Adorf <csadorf@umich.edu>
2016-02-23 19:16:06 -0500

    Add mdtraj to full conda test environments.

--- conda_create_test_env.sh
+++ conda_create_test_env.sh
@@ -41,6 +41,7 @@ echo "Done."
 echo "Creating environment for python 2.7"
 conda create --yes -n glotzformats-py27 python=2.7 numpy
 . activate glotzformats-py27
+conda install --yes -c omnia mdtraj
 pip install git+ssh://git@bitbucket.org/glotzer/libgetar.git#egg=libgetar
 python setup.py develop
 . deactivate
@@ -49,6 +50,7 @@ echo "Done."
 echo "Creating environment for python 3.3"
 conda create --yes -n glotzformats-py33 python=3.3 numpy
 . activate glotzformats-py33
+conda install --yes -c omnia mdtraj
 pip install git+ssh://git@bitbucket.org/glotzer/libgetar.git#egg=libgetar
 python setup.py develop
 . deactivate
@@ -57,6 +59,7 @@ echo "Done."
 echo "Creating environment for python 3.4"
 conda create --yes -n glotzformats-py34 python=3.4 numpy
 . activate glotzformats-py34
+conda install --yes -c omnia mdtraj
 pip install git+ssh://git@bitbucket.org/glotzer/libgetar.git#egg=libgetar
 python setup.py develop
 . deactivate
@@ -65,6 +68,7 @@ echo "Done."
 echo "Creating environment for python 3.5"
 conda create --yes -n glotzformats-py35 python=3.5 numpy
 . activate glotzformats-py35
+conda install --yes -c omnia mdtraj
 pip install git+ssh://git@bitbucket.org/glotzer/libgetar.git#egg=libgetar
 python setup.py develop
 . deactivate
