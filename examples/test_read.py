5effae7806e438fe476e84b9efd257595139bffa
Carl Simon Adorf <csadorf@umich.edu>
2016-02-23 19:31:23 -0500

    Add hoomd-blue xml and dcd read test to test_read.py script.

--- examples/test_read.py
+++ examples/test_read.py
@@ -12,10 +12,16 @@ except ImportError:
     GTAR = False
 else:
     GTAR = True
+try:
+    import mdtraj
+except ImportError:
+    MDTRAJ=False
+else:
+    MDTRAJ=True
 
 
-def test_read(file, reader):
-    traj = reader.read(file)
+def test_read(file, reader, *args, **kwargs):
+    traj = reader.read(file, *args, **kwargs)
     for frame in traj:
         frame.load()
         print(frame)
@@ -30,6 +36,15 @@ def main():
         for fn in glob.glob('../samples/*.tar'):
             with open(fn, 'rb') as file:
                 test_read(file, glotzformats.reader.GetarFileReader())
+    if MDTRAJ:
+        for fn in glob.glob('../samples/*.dcd'):
+            with open(glob.glob('../samples/*.xml')[0]) as xmlfile:
+                frame = glotzformats.reader.HoomdBlueXMLReader().read(xmlfile)[0]
+                with open(fn, 'rb') as file:
+                    test_read(file, glotzformats.reader.DCDReader(), frame)
+    for fn in glob.glob('../samples/*.xml'):
+        with open(fn) as file:
+            test_read(file, glotzformats.reader.HoomdBlueXMLReader())
     for fn in glob.glob('../samples/*.pos'):
         with open(fn) as file:
             test_read(file, glotzformats.reader.PosFileReader())
