8710cc5c4556f3c411c5cfceaa93e4eab56c7245
Carl Simon Adorf <csadorf@umich.edu>
2016-02-23 20:32:43 -0500

    Bump version to 0.1.7 alpha release.

--- src/glotzformats/__init__.py
+++ src/glotzformats/__init__.py
@@ -9,6 +9,6 @@ from . import writer
 from . import samples
 from . import trajectory
 
-__version__ = '0.1.6'
+__version__ = '0.1.7a1'
 
 __all__ = ['formats', 'reader', 'writer', 'samples', 'trajectory']
