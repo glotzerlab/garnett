14941c119f74d54c9fad7aedd25385e4141a4da6
Carl Simon Adorf <csadorf@umich.edu>
2016-02-27 10:27:17 -0500

    3rd 0.1.7 alpha release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if not sys.version_info >= (2,7):
 
 setup(
     name = 'glotz-formats',
-    version = '0.1.7a2',
+    version = '0.1.7a3',
     package_dir = {'': 'src'},
     packages = find_packages('src'),
 
