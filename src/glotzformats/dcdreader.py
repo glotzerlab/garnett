4708aa5879731b47655146ca369935ed6ad20507
Carl Simon Adorf <csadorf@umich.edu>
2016-03-24 14:37:41 -0400

    Implement dcd-reader (tested).

--- src/glotzformats/dcdreader.py
+++ src/glotzformats/dcdreader.py
@@ -25,7 +25,6 @@ import struct
 import copy
 
 import numpy as np
-#import mdtraj as md
 
 from .trajectory import _RawFrameData, Frame, Trajectory
 from .errors import ParserError
@@ -77,12 +76,13 @@ def skip_frame(file):
 def box_matrix_from_frame_header(frame_header, tol=1e-12):
     from math import sin, cos, sqrt, pi
     fh = frame_header
+
     def almost_zero(r):
         return 0.0 if r < tol else r
 
-    alpha = fh.box_alpha or pi/2
-    beta = fh.box_beta or pi/2
-    gamma = fh.box_gamma or pi/2
+    alpha = fh.box_alpha or pi / 2
+    beta = fh.box_beta or pi / 2
+    gamma = fh.box_gamma or pi / 2
     a = [fh.box_a, 0, 0]
     b = [almost_zero(fh.box_b * cos(gamma)), fh.box_b * sin(gamma), 0]
     c1 = fh.box_c * cos(beta)
@@ -91,20 +91,23 @@ def box_matrix_from_frame_header(frame_header, tol=1e-12):
     c = [almost_zero(c1), almost_zero(c2), c3]
     return [a, b, c]
 
+
 class DCDFrame(Frame):
 
-    def __init__(self, file, file_header, frame_header, start, t_frame):
+    def __init__(self, file, file_header, frame_header, start,
+                 t_frame, default_type='A'):
         self.file = file
         self.file_header = file_header
         self.frame_header = frame_header
         self.start = start
         self.t_frame = t_frame
+        self.default_type = default_type
         super(DCDFrame, self).__init__()
 
     def read(self):
         raw_frame = copy.deepcopy(self.t_frame)
-        assert len(raw_frame.positions) == self.file_header.n_particles
-        raw_frame.box = np.asarray(box_matrix_from_frame_header(self.frame_header)).T
+        raw_frame.box = np.asarray(
+            box_matrix_from_frame_header(self.frame_header)).T
         self.file.seek(self.start)
         N = self.file_header.n_particles
         xyz = []
@@ -112,31 +115,16 @@ class DCDFrame(Frame):
             len_section = read_int(self.file)
             xyz.append([read_float(self.file) for j in range(N)])
             assert read_int(self.file) == len_section
-        print(np.array(xyz).T)
         raw_frame.positions = np.array(xyz).T
+        raw_frame.orientations = np.zeros((len(raw_frame.positions), 4))
+        raw_frame.types = [self.default_type] * len(raw_frame.positions)
+        assert len(raw_frame.positions) == self.file_header.n_particles
         return raw_frame
 
     def __str__(self):
-        return "DCDFrame(# frames={}, topology_frame={})".format(len(self.traj), self.t_frame)
+        return "DCDFrame(# frames={}, topology_frame={})".format(
+            len(self.traj), self.t_frame)
 
-
-# class DCDFrame(Frame):
-#
-#     def __init__(self, traj, frame_index, t_frame):
-#         # This implementation requires a 3rd party reader
-#         self.traj=traj
-#         self.frame_index = frame_index
-#         self.t_frame = t_frame
-#         super(DCDFrame, self).__init__()
-#
-#     def read(self):
-#         "Read the frame data from the file."
-#         raw_frame = copy.deepcopy(self.t_frame)
-#         raw_frame.box = np.asarray(raw_frame.box.get_box_matrix())
-#         B = raw_frame.box
-#         p = self.traj.xyz[self.frame_index]
-#         raw_frame.positions = [2*np.dot(B, p_) for p_ in p]
-#         return raw_frame
 
 class DCDFileReader(object):
     """Read dcd trajectory files."""
@@ -159,7 +147,7 @@ class DCDFileReader(object):
         assert read_int(file) == 84
         title_section_size = read_int(file)
         n_titles = read_int(file)
-        len_title = int((title_section_size-4) / 2)
+        len_title = int((title_section_size - 4) / 2)
         file_header.titles = []
         for i in range(n_titles):
             file_header.titles.append(file.read(len_title).decode('ascii'))
@@ -169,36 +157,37 @@ class DCDFileReader(object):
         assert read_int(file) == 4
         return file_header
 
-    def _scan(self, file, t_frame=None):
-        file_header = self._read_file_header(file)
+    def _scan(self, file, t_frame=None, default_type='A'):
+        try:
+            file_header = self._read_file_header(file)
+        except Exception as error:
+            raise ParserError(
+                "Failed to read dcd file header: '{}'.".format(error))
         for i in range(file_header.num_frames):
-            frame_header = read_frame_header(file)
-            yield DCDFrame(file=file, file_header=file_header, frame_header=frame_header, start=file.tell(), t_frame=t_frame)
-            skip_frame(file)
+            try:
+                frame_header = read_frame_header(file)
+                yield DCDFrame(file=file, file_header=file_header,
+                               frame_header=frame_header, start=file.tell(),
+                               t_frame=t_frame, default_type=default_type)
+                skip_frame(file)
+            except Exception as error:
+                raise ParserError(
+                    "Failed to read frame #{}: '{}'.".format(i, error))
 
-
-    def read(self, file, frame=None):
+    def read(self, file, frame=None, default_type='A'):
         """Read binary file and return a trajectory instance.
 
         :param file: The file, which contains the xmlfile.
         :type file: A file-like textfile.
         :param frame: A frame containing topological information
             that cannot be encoded in the dcd-format.
-        :type frame: :class:`trajectory.Frame`"""
+        :type frame: :class:`trajectory.Frame`
+        :param default_type: A type name to be used when no first
+            frame is provided.
+        :type default_type: str"""
         if frame is None:
             frame = _RawFrameData()
-        frames = list(self._scan(file, t_frame=frame))
+        frames = list(self._scan(file, t_frame=frame,
+                                 default_type=default_type))
         logger.info("Read {} frames.".format(len(frames)))
         return Trajectory(frames)
-#
-#
-#        try:
-#            top = md.Topology()
-#            [top.add_atom(t, 'X', top.add_residue('x', top.add_chain())) for t in frame.types]
-#            mdtraj = md.load_dcd(file.name, top=top)
-#        except AttributeError:
-#            raise
-#        else:
-#            frames = [DCDFrame(mdtraj, i, frame) for i in range(len(mdtraj))]
-#            logger.info("Read {} frames.".format(len(frames)))
-#            return Trajectory(frames)
