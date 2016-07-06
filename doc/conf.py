a72d34388f07b444dc8ace9490f1e5df1432ce4e
Carl Simon Adorf <csadorf@umich.edu>
2016-07-05 22:37:31 -0400

    Update copyright value in doc conf.

--- doc/conf.py
+++ doc/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'glotzformats'
-copyright = '2015, Carl Simon Adorf'
+copyright = '2015-2016, Carl Simon Adorf'
 author = 'Carl Simon Adorf'
 
 # The version info for the project you're documenting, acts as replacement for
