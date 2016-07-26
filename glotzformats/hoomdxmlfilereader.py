1719805818edfc8d180e14a0fa215a3931d40811
Carl Simon Adorf <csadorf@umich.edu>
2016-07-26 17:35:28 -0400

    Fix formatting.

--- glotzformats/hoomdxmlfilereader.py
+++ glotzformats/hoomdxmlfilereader.py
@@ -39,7 +39,8 @@ class HOOMDXMLFrame(Frame):
         if velocities is None:
             raw_frame.velocities = [[0, 0, 0]] * len(raw_frame.positions)
         else:
-            raw_frame.velocities = list(_parse_velocities(config.find('velocity')))
+            raw_frame.velocities = list(
+                _parse_velocities(config.find('velocity')))
         orientations = config.find('orientation')
         if orientations is None:
             raw_frame.orientations = [[1, 0, 0, 0]] * len(raw_frame.positions)
@@ -91,20 +92,23 @@ def _get_box_matrix(box):
 def _parse_positions(positions):
     for i, position in enumerate(positions.text.splitlines()[1:]):
         yield [float(x) for x in position.split()]
-    if i + 1 != int(positions.attrib.get('num', i+1)):
+    if i + 1 != int(positions.attrib.get('num', i + 1)):
         warnings.warn("Number of positions inconsistent.")
+
 
 def _parse_velocities(velocities):
     for i, velocity in enumerate(velocities.text.splitlines()[1:]):
         yield [float(x) for x in velocity.split()]
-    if i + 1 != int(velocities.attrib.get('num', i+1)):
+    if i + 1 != int(velocities.attrib.get('num', i + 1)):
         warnings.warn("Number of velocities inconsistent.")
+
 
 def _parse_orientations(orientations):
     for i, orientation in enumerate(orientations.text.splitlines()[1:]):
         yield [float(x) for x in orientation.split()]
-    if i + 1 != int(orientations.attrib.get('num', i+1)):
+    if i + 1 != int(orientations.attrib.get('num', i + 1)):
         warnings.warn("Number of orientations inconsistent.")
+
 
 def _parse_types(types):
     for i, type in enumerate(types.text.splitlines()[1:]):
