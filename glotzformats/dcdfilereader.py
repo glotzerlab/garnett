Fix quaternion to euler angle conversion example.
This is an error in the dcd file reader documentation.
Contribution by Eric Harper.

glotzformats/dcdfilereader.py
@@ -309,7 +309,7 @@ class _DCDFileReader(object):
 
         .. code::
 
-            alpha = 2 * np.arccos(traj[0].orientations.T[0])
+            alpha = 2 * np.arctan2(ort[:, 3], ort[:, 0])
     """
     _dcdreader = pydcdreader
 