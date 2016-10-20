1cc2c0e100bed4d1a73d3462fb55c67c884748c6
Carl Simon Adorf <csadorf@umich.edu>
2016-10-20 14:25:32 -0400

    Bump up version number to 0.3.6.

--- glotzformats/__init__.py
+++ glotzformats/__init__.py
@@ -9,6 +9,6 @@ from . import writer
 from . import samples
 from . import trajectory
 
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 
 __all__ = ['formats', 'reader', 'writer', 'samples', 'trajectory']
