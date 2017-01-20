9142da682496122d240559b95781a5657619f783
Chengyu Dai <jdaaph@gmail.com>
2017-01-20 14:39:49 -0500

    tidy up

--- test.py
+++
@@ -1,9 +0,0 @@
-import glotzformats as gf
-
-r = gf.reader.GSDHOOMDFileReader()
-with open('../dump.gsd', 'rb') as f:
-    t = r.read(f)
-    print(dir(t[30]))
-    # print(t[30].orientations)
-    print(t[30].angmoms)
-    
