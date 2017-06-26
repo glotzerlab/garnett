e04e6f938cbb8acb882b827aad3aaf2154138eac
Carl Simon Adorf <csadorf@umich.edu>
2017-06-26 14:20:11 -0400

    Mock gsd package in doc conf.
    To avoid requirement of gsd for building of documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -296,4 +296,4 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-autodoc_mock_imports = ['numpy', 'numpy.core', 'numpy.core.numeric','dcdreader', 'gtar', 'CifFile']
+autodoc_mock_imports = ['numpy', 'numpy.core', 'numpy.core.numeric','dcdreader', 'gtar', 'CifFile', 'gsd', 'gsd.hoomd']
