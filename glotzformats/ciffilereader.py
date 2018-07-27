74fe78d046476c9637aa2c1b5448dc76e5d5bc83
Matthew Spellings <mspells@umich.edu>
2018-07-27 09:52:12 -0400

    Use atom_site_type from cif files when available

--- glotzformats/ciffilereader.py
+++ glotzformats/ciffilereader.py
@@ -91,6 +91,15 @@ class CifFileFrame(Frame):
                               for (x, y, z) in zip(
             self.parsed['_atom_site_fract_x'], self.parsed['_atom_site_fract_y'], self.parsed['_atom_site_fract_z'])])
 
+
+        if '_atom_site_type_symbol' in self.parsed:
+            site_types = list(self.parsed['_atom_site_type_symbol'])
+        elif '_atom_site_label' in self.parsed:
+            site_types = [re.search('([a-zA-Z]+)', label)
+                          for label in self.parsed['_atom_site_label']]
+        else:
+            site_types = len(fractions)*[self.default_type]
+
         if '_symmetry_equiv_pos_as_xyz' in self.parsed:
             symmetry_ops = [PARSE_DIVISION_REGEXP.sub(
                             _parse_division, REMOVE_NONNUM_REGEXP.sub('', sym))
@@ -98,7 +107,7 @@ class CifFileFrame(Frame):
 
             replicated_fractions = []
             replicated_types = []
-            for (typ, (fx, fy, fz)) in zip(self.parsed['_atom_site_label'], fractions):
+            for (typ, (fx, fy, fz)) in zip(site_types, fractions):
                 extra_fractions = [eval(sym, dict(x=fx, y=fy, z=fz)) for sym in symmetry_ops]
                 replicated_fractions.extend(extra_fractions)
                 replicated_types.extend(len(extra_fractions)*[typ])
@@ -138,10 +147,7 @@ class CifFileFrame(Frame):
                 unique_types = types
         else:
             unique_points = fractions
-            if '_atom_site_label' in self.parsed:
-                unique_types = self.parsed['_atom_site_label']
-            else:
-                unique_types = len(unique_points)*[self.default_type]
+            unique_types = site_types
 
         # shift so that (0, 0, 0) in fractional coordinates goes to a
         # corner of the box, not the center of the box
