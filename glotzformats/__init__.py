c42229fdc7b3c67f0a27f12d0f7517e2aacd33fb
Bradley Dice <bdice@bradleydice.com>
2018-08-16 13:59:43 -0400

    Made some suggested changes.

--- glotzformats/__init__.py
+++ glotzformats/__init__.py
@@ -8,8 +8,8 @@ from . import reader
 from . import writer
 from . import samples
 from . import trajectory
-from .reader_function import autoread ## to make a shortcut syntax when reading files having particle data. (glotzformats.autoread('filename.ext'))
+from .util import read
 
 __version__ = '0.4.1'
 
-__all__ = ['formats', 'reader', 'writer', 'samples', 'trajectory','autoread']
+__all__ = ['formats', 'reader', 'writer', 'samples', 'trajectory', 'read']
---
+++ glotzformats/util.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2018 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+
+"""Utility functions for format detection and conversion."""
+import os
+import logging
+from . import reader, trajectory
+
+
+logger = logging.getLogger(__name__)
+
+
+# Mapping of file extension to format
+FORMATS = {
+    '.pos': 'pos',
+    '.gsd': 'gsd',
+    '.zip': 'gtar',
+    '.tar': 'gtar',
+    '.sqlite': 'gtar',
+    '.dcd': 'dcd',
+    '.xml': 'xml',
+    '.cif': 'cif'}
+
+# Mapping of format to readers and input modes
+READ_CLASS_MODES = {
+    'pos': {
+        'reader': reader.PosFileReader,
+        'mode': 'r'},
+    'gsd': {
+        'reader': reader.GSDHOOMDFileReader,
+        'mode': 'rb'},
+    'gtar': {
+        'reader': reader.GetarFileReader,
+        'mode': 'rb'},
+    'dcd': {
+        'reader': reader.DCDFileReader,
+        'mode': 'rb'},
+    'xml': {
+        'reader': reader.HOOMDXMLFileReader,
+        'mode': 'r'},
+    'cif': {
+        'reader': reader.CifFileReader,
+        'mode': 'r'}}
+
+
+def detect_format(filename):
+    extension = os.path.splitext(filename)[1]
+    try:
+        file_format = FORMATS[extension]
+    except KeyError:
+        raise ValueError(
+            'The extension "{}" is not recognized.'.format(extension))
+    return file_format
+
+
+def read(filename, template=None, frames=None):
+    """This function automatically detects the file format, read the file, and
+    returns a trajectory object.
+
+    :param filename: Filename to read.
+    :param template: Optional template for the GSDHOOMDFileReader.
+    :param frames: Optional integer or slice object to subset the trajectory.
+    :type filename: string
+    :type template: string
+    :type frames: int or slice
+    :returns: Trajectory read from the file.
+    :rtype: :class:`glotzformats.trajectory.Trajectory`
+    """
+
+    if not os.path.isfile(filename):
+        raise FileNotFoundError('Filename {} cannot be found.'.format(
+            filename))
+
+    file_format = detect_format(filename)
+    reader = READ_CLASS_MODES[file_format]['reader']()
+    mode = READ_CLASS_MODES[file_format]['mode']
+
+    with open(filename, mode) as read_file:
+        if file_format == 'gsd':
+            traj = reader.read(read_file, template)
+        else:
+            traj = reader.read(read_file)
+
+        if frames is not None:
+            if type(frames) is int or type(frames) is slice:
+                traj = traj[frames]
+            else:
+                raise ValueError('Argument frames must be an int or slice.')
+
+        if not isinstance(traj, trajectory.Trajectory):
+            traj = trajectory.Trajectory([traj])
+        traj.load_arrays()
+
+    return traj
