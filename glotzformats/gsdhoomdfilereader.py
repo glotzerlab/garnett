2c6a305e8da9834991d9b02b0c3e08a20011d565
Bradley Dice <bdice@bradleydice.com>
2019-02-28 14:16:44 -0800

    Fix polygon shape parsing for GSDHOOMDFileReader.

--- glotzformats/gsdhoomdfilereader.py
+++ glotzformats/gsdhoomdfilereader.py
@@ -115,7 +115,8 @@ def _parse_shape_definitions(frame, gsdfile, frame_index):
         verts_split = [verts[start:end] for start, end in zip(N_start, N_end)]
         for typename, typeverts in zip(types, verts_split):
             shapedefs[typename] = PolygonShape(
-                vertices=[typeverts[0], typeverts[1], 0], color=None)
+                vertices=[[v[0], v[1], 0] for v in typeverts],
+                color=None)
         return shapedefs
 
     # Convex Spheropolygons
@@ -129,7 +130,7 @@ def _parse_shape_definitions(frame, gsdfile, frame_index):
                                 'state/hpmc/convex_spheropolygon/sweep_radius')
         for typename, typeverts, radius in zip(types, verts_split, sweep_radii):
             shapedefs[typename] = SpheropolygonShape(
-                vertices=[typeverts[0], typeverts[1], 0],
+                vertices=[[v[0], v[1], 0] for v in typeverts],
                 rounding_radius=radius, color=None)
         return shapedefs
 
