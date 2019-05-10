a78bd5695a39cf6d402d97be8f6947559c5dcba3
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2019-05-10 18:18:16 -0400

    added raise attribute error unit test to dcd reader

--- tests/test_dcdfilereader.py
+++ tests/test_dcdfilereader.py
@@ -43,6 +43,22 @@ class BaseDCDFileReaderTest(TrajectoryTest):
         top_traj = self.read_top_trajectory()
         return self.reader().read(self.get_sample_file(), top_traj[0])
 
+    def assert_raise_attribute_error(self,frame):
+        with self.assertRaises(AttributeError):
+            frame.types;
+            frame.orientations;
+            frame.charge;
+            frame.diameter;
+            frame.moment_inertia;
+            frame.angmom;
+            frame.image;
+            frame.data;
+            frame.data_keys;
+            frame.shapedef;
+            frame.view_rotation;
+            frame.mass;
+            frame.velocities;
+
     def test_read(self):
         assert self.read_top_trajectory()
         traj = self.get_traj()
@@ -67,6 +83,8 @@ class BaseDCDFileReaderTest(TrajectoryTest):
             [1.78225398064, -0.266534328461, -1.39306223392],
             [0.162209749222, -2.22765517235, -1.27463591099]])))
 
+        for frame in traj:
+            self.assert_raise_attribute_error(frame);
 
 if __name__ == '__main__':
     unittest.main()
