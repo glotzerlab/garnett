d5556ae58a2bbfe57dc945943e3c4fedb947b943
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2019-05-13 14:12:23 -0400

    fix syntax error

--- glotzformats/gsdhoomdfilereader.py
+++ glotzformats/gsdhoomdfilereader.py
@@ -215,7 +215,7 @@ class GSDHOOMDFileReader(object):
 
     def __init__(self, read_gsd_shape_data=None):
         if read_gsd_shape_data is not None:
-            warnings.warn("Ignoring read_gsd_shape_data",warnings.DeprecationWarning)
+            warnings.warn("Ignoring read_gsd_shape_data", DeprecationWarning)
 
     def read(self, stream, frame=None):
         """Read binary stream and return a trajectory instance.
