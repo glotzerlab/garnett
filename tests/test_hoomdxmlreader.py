87aba96c278e7dfe6c7acf513dcad5a2e2adff97
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2019-06-13 09:51:59 -0400

    added 2d test for xml reader

--- tests/test_hoomdxmlreader.py
+++ tests/test_hoomdxmlreader.py
@@ -12,17 +12,16 @@ PYTHON_2 = sys.version_info[0] == 2
 
 class BaseHOOMDXMLFileReaderTest(unittest.TestCase):
 
-    def read_trajectory(self):
+    def read_trajectory(self, sample_file):
         reader = glotzformats.reader.HOOMDXMLFileReader()
         if PYTHON_2:
-            return reader.read(io.StringIO(
-                unicode(glotzformats.samples.HOOMD_BLUE_XML)))  # noqa
+            return reader.read(io.StringIO(unicode(sample_file)))  # noqa
         else:
             return reader.read(
-                io.StringIO(glotzformats.samples.HOOMD_BLUE_XML))
+                io.StringIO(sample_file))
 
-    def test_read(self):
-        traj = self.read_trajectory()
+    def test_read_3d(self):
+        traj = self.read_trajectory(glotzformats.samples.HOOMD_BLUE_XML)
         self.assertEqual(len(traj), 1)
         self.assertEqual(len(traj[0]), 10)
         self.assertTrue(np.allclose(traj[0].positions, np.array([
@@ -56,6 +55,45 @@ class BaseHOOMDXMLFileReaderTest(unittest.TestCase):
                 [0, 4.713492870331, 0],
                 [0, 0, 4.713492870331]])))
 
+        self.assertTrue(np.all([ frame.box.dimensions == 3 for frame in traj]))
+
+
+    def test_read_2d(self):
+           traj = self.read_trajectory(glotzformats.samples.HOOMD_BLUE_XML_2D)
+           self.assertEqual(len(traj), 1)
+           self.assertEqual(len(traj[0]), 10)
+           self.assertTrue(np.allclose(traj[0].positions, np.array([
+               [-0.391116261482, 2.34347701073, 0],
+               [2.03885579109, -2.35620737076, 0],
+               [-0.931703925133, 2.35222411156, 0],
+               [-1.24394273758, -1.92150914669, 0],
+               [-1.47881031036, -0.528331875801,0],
+               [0.800096750259, -0.486585855484, 0],
+               [0.182962417603, 1.63233399391, 0],
+               [-0.880133986473, 0.873030900955, 0],
+               [-1.39306223392, -0.266534328461, 0],
+               [-1.27463591099, -2.22765517235, 0],
+           ])))
+           self.assertTrue(np.allclose(traj[0].velocities, np.array([
+               [-0.391116261482, 2.34347701073, 0],
+               [2.03885579109, -2.35620737076, 0],
+               [-0.931703925133, 2.35222411156, 0],
+               [-1.24394273758, -1.92150914669, 0],
+               [-1.47881031036, -0.528331875801, 0],
+               [0.800096750259, -0.486585855484, 0],
+               [0.182962417603, 1.63233399391, 0],
+               [-0.880133986473, 0.873030900955, 0],
+               [-1.39306223392, -0.266534328461, 0],
+               [-1.27463591099, -2.22765517235, 0],
+           ])))
+           self.assertTrue(np.allclose(
+               np.asarray(traj[0].box.get_box_matrix()),
+               np.array([
+                   [4.713492870331, 0, 0],
+                   [0, 4.713492870331, 0],
+                   [0, 0, 1.0]])))
+
+           self.assertTrue(np.all([ frame.box.dimensions == 2 for frame in traj]))
 
 if __name__ == '__main__':
     unittest.main()
