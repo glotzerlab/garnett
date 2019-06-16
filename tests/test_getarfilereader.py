79980acfc7f4f70b2465de4316161ef583adbf96
Kelly Wang <kelwang@umich.edu>
2019-06-16 13:19:10 -0400

    Changed random int function to return more values.

--- tests/test_getarfilereader.py
+++ tests/test_getarfilereader.py
@@ -37,7 +37,7 @@ class BaseGetarFileReaderTest(unittest.TestCase):
         self.diameter = np.random.rand(N)
         self.moment_inertia = np.random.rand(N, 3)
         self.angmom = np.random.rand(N, 4)
-        self.image = np.random.randint(1, size=(N, 3))
+        self.image = np.random.randint(-1000, 1000, size=(N, 3))
         types = N // 2 * [0] + (N - N // 2) * [1]
         type_names = ['A', 'B']
         self.box = np.array([1.0, 1.0, 1.0, 0.0, 0.0, 0.0])
