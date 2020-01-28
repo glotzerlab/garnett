774b06b68ba8612807af4f0c9fbef358e0d0b3c0
Luis Y. Rivera-Rivera <lyrivera@umich.edu>
2020-01-28 14:30:40 -0500

    added version file

---
+++ garnett/version.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2019 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+
+
+__version__ = "1.3.0"
+
+
+
+__all__ = [
+    '__version__'
+]
