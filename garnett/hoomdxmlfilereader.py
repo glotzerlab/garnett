722958f433b4729fa5c8671d4836803d83960fd7
Bradley Dice <bdice@bradleydice.com>
2020-02-23 00:58:39 -0500

    Update types, typeid, type_shapes in HOOMD XML file reader/writer.

--- garnett/hoomdxmlfilereader.py
+++ garnett/hoomdxmlfilereader.py
@@ -45,7 +45,7 @@ class HOOMDXMLFrame(Frame):
         if velocity is not None:
             raw_frame.velocity = list(_parse_velocity(velocity))
 
-        raw_frame.types = list(_parse_types(config.find('type')))
+        raw_frame.types, raw_frame.typeid = _parse_types(config.find('type'))
         return raw_frame
 
     def __str__(self):
@@ -109,8 +109,14 @@ def _parse_orientation(orientation):
         warnings.warn("Number of orientations is inconsistent.")
 
 
-def _parse_types(types):
-    for i, type in enumerate(types.text.splitlines()[1:]):
-        yield str(type)
-    if i + 1 != int(types.attrib.get('num', i)):
+def _parse_types(types_element):
+    types = []
+    typeid = []
+    for typ in types_element.text.splitlines()[1:]:
+        typ = str(typ)
+        if typ not in types:
+            types.append(typ)
+        typeid.append(types.index(typ))
+    if len(typeid) != int(types_element.attrib.get('num', -1)):
         warnings.warn("Number of types is inconsistent.")
+    return types, typeid
